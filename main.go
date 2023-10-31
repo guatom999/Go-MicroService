aeadf80a6ddecbf5308661cb4430f85f2672793a
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-10-31 11:44:15 +0700

    create config file

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"os"
 
+	"github.com/guatom999/Go-MicroService/config"
+)
+
+func main() {
+	ctx := context.Background()
+	_ = ctx
+
+	cfg := config.LoadConfig(func() string {
+		if len(os.Args) < 2 {
+			log.Fatal("Error: .env path is required")
+		}
+		return os.Args[1]
+	}())
+
+	log.Panicln(cfg)
 }
