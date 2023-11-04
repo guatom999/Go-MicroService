dea6a043187a5af21dcd05c18b7315091e3a3b91
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-11-04 14:22:17 +0700

    create utils func

--- pkg/utils/helper.go
+++ pkg/utils/helper.go
@@ -1,1 +1,11 @@
 package utils
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func Debug(obj any) {
+	raw, _ := json.MarshalIndent(obj, "", "\t")
+	fmt.Println(string(raw))
+}
---
+++ pkg/utils/mongo.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func ConvertToObject(id string) primitive.ObjectID {
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	return objectId
+}
+
+func LocalTime() time.Time {
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+	return time.Now().In(loc)
+}
+
+func ConvertStringTimeToTime(t string) time.Time {
+	layout := "2006-01-02T15:04:05.999 -0700 MST"
+	result, err := time.Parse(layout, t)
+
+	if err != nil {
+		log.Printf("Error : Parse Time failed: %s", err.Error())
+	}
+
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+
+	return result.In(loc)
+}
