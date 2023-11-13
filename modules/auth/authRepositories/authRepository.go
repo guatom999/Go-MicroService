104deb556c3f4c11e67df13ead16c5234fe6ed84
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-11-13 23:57:10 +0700

    auth module repo

--- modules/auth/authRepositories/authRepository.go
+++ modules/auth/authRepositories/authRepository.go
@@ -2,12 +2,22 @@ package authRepositories
 
 import (
 	"context"
+	"errors"
+	"log"
+	"time"
 
+	"github.com/guatom999/Go-MicroService/modules/auth"
+	playerPb "github.com/guatom999/Go-MicroService/modules/player/playerPb"
+	"github.com/guatom999/Go-MicroService/pkg/grpccon"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type (
 	IAuthRepositoryService interface {
+		CredentialSearch(pctx context.Context, grpcUrl string, req *playerPb.CredetialSearchReq) (*playerPb.PlayerProfile, error)
+		InsertOnePlayerCredential(pctx context.Context, req *auth.Credential) (primitive.ObjectID, error)
 	}
 
 	authRepository struct {
@@ -21,4 +31,42 @@ func NewAuthRepository(db *mongo.Client) IAuthRepositoryService {
 
 func (r *authRepository) authDbConn(pctx context.Context) *mongo.Database {
 	return r.db.Database("auth_db")
+}
+
+func (r *authRepository) CredentialSearch(pctx context.Context, grpcUrl string, req *playerPb.CredetialSearchReq) (*playerPb.PlayerProfile, error) {
+
+	ctx, cancel := context.WithTimeout(pctx, time.Second*30)
+	defer cancel()
+
+	conn, err := grpccon.NewGrpcClient(grpcUrl)
+	if err != nil {
+		log.Printf("Error: grpc connection failed: %s", err.Error())
+		return nil, errors.New("error: grpc connection failed")
+	}
+
+	result, err := conn.Player().CredetialSearch(ctx, req)
+	if err != nil {
+		log.Printf("Error: credential search  failed: %s", err.Error())
+		return nil, errors.New(err.Error())
+	}
+
+	return result, nil
+}
+
+func (r *authRepository) InsertOnePlayerCredential(pctx context.Context, req *auth.Credential) (primitive.ObjectID, error) {
+
+	ctx, cancel := context.WithTimeout(pctx, time.Second*10)
+	defer cancel()
+
+	db := r.authDbConn(ctx)
+	col := db.Collection("auth")
+
+	result, err := col.InsertOne(ctx, req)
+
+	if err != nil {
+		log.Printf("Error: InsertOnePlayerCredential  failed: %s", err.Error())
+		return primitive.NilObjectID, errors.New("error: insert one player credential failed")
+	}
+
+	return result.InsertedID.(primitive.ObjectID), nil
 }
