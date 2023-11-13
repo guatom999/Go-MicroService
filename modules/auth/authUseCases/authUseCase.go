ba7db6a8e3e7b8a1ba09daee50c077c38fd84929
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-11-13 23:57:17 +0700

    auth module usecase

--- modules/auth/authUseCases/authUseCase.go
+++ modules/auth/authUseCases/authUseCase.go
@@ -1,9 +1,21 @@
 package authUseCases
 
-import "github.com/guatom999/Go-MicroService/modules/auth/authRepositories"
+import (
+	"context"
+	"time"
+
+	"github.com/guatom999/Go-MicroService/config"
+	"github.com/guatom999/Go-MicroService/modules/auth"
+	"github.com/guatom999/Go-MicroService/modules/auth/authRepositories"
+	"github.com/guatom999/Go-MicroService/modules/player"
+	playerPb "github.com/guatom999/Go-MicroService/modules/player/playerPb"
+	"github.com/guatom999/Go-MicroService/pkg/jwtauth"
+	"github.com/guatom999/Go-MicroService/pkg/utils"
+)
 
 type (
 	IAuthUseCaseService interface {
+		Login(pctx context.Context, cfg *config.Config, req *auth.PlayerLoginReq) (*auth.ProfileIntercepter, error)
 	}
 
 	authUseCase struct {
@@ -13,4 +25,52 @@ type (
 
 func NewAuthUseCase(authRepo authRepositories.IAuthRepositoryService) IAuthUseCaseService {
 	return &authUseCase{authRepo: authRepo}
+}
+
+func (u *authUseCase) Login(pctx context.Context, cfg *config.Config, req *auth.PlayerLoginReq) (*auth.ProfileIntercepter, error) {
+
+	profile, err := u.authRepo.CredentialSearch(pctx, cfg.Grpc.PlayerUrl, &playerPb.CredetialSearchReq{
+		Email:    req.Email,
+		Password: req.Password,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	profile.Id = "player:" + profile.Id
+
+	accessToken := jwtauth.NewAccessToken(cfg.Jwt.AccessSecretKey, cfg.Jwt.AccessDuration, &jwtauth.Claims{
+		PlayerId: profile.Id,
+		RoleCode: int(profile.RoleCode),
+	}).SignToken()
+
+	refreshToken := jwtauth.NewRefreshToken(cfg.Jwt.RefreshSecretKey, cfg.Jwt.RefreshDuration, &jwtauth.Claims{
+		PlayerId: profile.Id,
+		RoleCode: int(profile.RoleCode),
+	}).SignToken()
+
+	credentialId, err := u.authRepo.InsertOnePlayerCredential(pctx, &auth.Credential{
+		PlayerId:     profile.Id,
+		RoldCode:     int(profile.RoleCode),
+		AccessToken:  accessToken,
+		ReFreshToken: refreshToken,
+		CreatedAt:    utils.LocalTime(),
+		UpdatedAt:    utils.LocalTime(),
+	})
+
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+
+	return &auth.ProfileIntercepter{
+		PlayerProfile: &player.PlayerProfile{
+			Id:        profile.Id,
+			Email:     profile.Email,
+			Username:  profile.Username,
+			CreatedAt: utils.ConvertStringTimeToTime(profile.CreatedAt).In(loc),
+			UpdatedAt: utils.ConvertStringTimeToTime(profile.UpdatedAt).In(loc),
+		},
+		Credential: &auth.CredentialRes{
+			Id: credentialId.Hex(),
+		},
+	}, nil
 }
