bedc11577a28656188dcfdd1fea58390df05bbdd
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-11-19 02:23:39 +0700

    insert item module

--- modules/item/itemRepositories/itemRepository.go
+++ modules/item/itemRepositories/itemRepository.go
@@ -2,12 +2,20 @@ package itemRepositories
 
 import (
 	"context"
+	"errors"
+	"log"
+	"time"
 
+	"github.com/guatom999/Go-MicroService/modules/item"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type (
 	IItemRepositoryService interface {
+		IsUniqueItem(pctx context.Context, title string) bool
+		InsertOneItem(pctx context.Context, req *item.Item) (primitive.ObjectID, error)
 	}
 
 	itemRepository struct {
@@ -21,4 +29,39 @@ func NewItemRepository(db *mongo.Client) IItemRepositoryService {
 
 func (r *itemRepository) itemDbConn(pctx context.Context) *mongo.Database {
 	return r.db.Database("item_db")
+}
+
+func (r *itemRepository) IsUniqueItem(pctx context.Context, title string) bool {
+
+	ctx, cancel := context.WithTimeout(pctx, 10*time.Second)
+	defer cancel()
+
+	db := r.itemDbConn(ctx)
+	col := db.Collection("items")
+
+	result := new(item.Item)
+
+	if err := col.FindOne(ctx, bson.M{"title": title}).Decode(result); err != nil {
+		log.Printf("Error: IsUnique Item %s", err.Error())
+	}
+
+	return true
+}
+
+func (r *itemRepository) InsertOneItem(pctx context.Context, req *item.Item) (primitive.ObjectID, error) {
+
+	ctx, cancel := context.WithTimeout(pctx, time.Second*15)
+	defer cancel()
+
+	db := r.itemDbConn(ctx)
+	col := db.Collection("items")
+
+	itemId, err := col.InsertOne(ctx, req)
+	if err != nil {
+		log.Printf("Error: InsertOneItem failed:%s", err)
+		return primitive.NilObjectID, errors.New("error: insert one item failed")
+	}
+
+	return itemId.InsertedID.(primitive.ObjectID), nil
+
 }
--- modules/item/itemUseCases/itemUseCase.go
+++ modules/item/itemUseCases/itemUseCase.go
@@ -1,6 +1,11 @@
 package itemUseCases
 
-import "github.com/guatom999/Go-MicroService/modules/item/itemRepositories"
+import (
+	"context"
+
+	"github.com/guatom999/Go-MicroService/modules/item"
+	"github.com/guatom999/Go-MicroService/modules/item/itemRepositories"
+)
 
 type (
 	IItemUseCaseService interface {
@@ -13,4 +18,8 @@ type (
 
 func NewItemUseCase(itemRepo itemRepositories.IItemRepositoryService) IItemUseCaseService {
 	return &itemUsecase{itemRepo: itemRepo}
+}
+
+func (u *itemUsecase) CreateItem(pctx context.Context, req *item.CreateItemReq) (string, error) {
+	return "", nil
 }
